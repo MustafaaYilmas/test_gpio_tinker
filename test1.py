85eefb2dd4870015020fe1672a940d350e6cdb65
Mustafa Yılmaz <121809092+MustafaaYilmas@users.noreply.github.com>
2023-07-12 16:23:48 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,37 +1,33 @@
+#Testing for software PWM
 import RPi.GPIO as GPIO
-import time
-
-RED_LED_PIN = 8
-GREEN_LED_PIN = 10
-BLUE_LED_PIN = 12
+import unittest   
+import time   
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(RED_LED_PIN, GPIO.OUT)
-GPIO.setup(GREEN_LED_PIN, GPIO.OUT)
-GPIO.setup(BLUE_LED_PIN, GPIO.OUT)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+redPwm = GPIO.PWM(8, 50)
+greenPwm = GPIO.PWM(10, 50)
+bluePwm = GPIO.PWM(12, 50)
+pwm.start(100)
+redPwm.ChangeDutyCycle(100)
+greenPwm.ChangeDutyCycle(100)
+bluePwm.ChangeDutyCycle(100)
 
-pwm_red = GPIO.PWM(RED_LED_PIN, 50)
-pwm_green = GPIO.PWM(GREEN_LED_PIN, 50)
-pwm_blue = GPIO.PWM(BLUE_LED_PIN, 50)
-
-try:
-    pwm_red.start(0) # Start red LED off
-    pwm_green.start(0) # Start green LED off
-    pwm_blue.start(0) # Start blue LED off
-
-    for pwm in [pwm_red, pwm_green, pwm_blue]:
-        # LED'i kademeli olarak parlaklaştırın
-        for brightness in range(0, 101, 5):
-            pwm.ChangeDutyCycle(brightness)
-            time.sleep(0.1)
-        # LED'i kademeli olarak karartın
-        for brightness in range(100, -1, -5):
-            pwm.ChangeDutyCycle(brightness)
-            time.sleep(0.1)
-        pwm.ChangeDutyCycle(0)  # Make sure the LED is turned off before moving to the next one
-
-finally:
-    pwm_red.stop()
-    pwm_green.stop()
-    pwm_blue.stop()
-    GPIO.cleanup()
+while True:
+    
+	for i in range(0,3):
+        redPwm.ChangeDutyCycle(0)
+        time.sleep(5)
+        redPwm.ChangeDutyCycle(100)
+        time.sleep(5)
+        greenPwm.ChangeDutyCycle(0)
+        time.sleep(5)
+        greenPwm.ChangeDutyCycle(100)
+        time.sleep(5)
+        bluePwm.ChangeDutyCycle(0)
+        time.sleep(5)
+        bluePwm.ChangeDutyCycle(100)
+        time.sleep(5)
+        
