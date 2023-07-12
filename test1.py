0dc72cab9ee7fdf7214759ec1a42ae032c56accc
Mustafa Yılmaz <121809092+MustafaaYilmas@users.noreply.github.com>
2023-07-12 13:26:59 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -10,12 +10,11 @@ GPIO.setup(RED_LED_PIN, GPIO.OUT)
 GPIO.setup(GREEN_LED_PIN, GPIO.OUT)
 GPIO.setup(BLUE_LED_PIN, GPIO.OUT)
 
-pwm_red = GPIO.PWM(RED_LED_PIN, 50)
-pwm_green = GPIO.PWM(GREEN_LED_PIN, 50)
-pwm_blue = GPIO.PWM(BLUE_LED_PIN, 50)
+leds = [RED_LED_PIN, GREEN_LED_PIN, BLUE_LED_PIN]
 
 try:
-    for pwm in [pwm_red, pwm_green, pwm_blue]:
+    for led in leds:
+        pwm = GPIO.PWM(led, 50)
         pwm.start(0)
         for i in range(3):
             for brightness in range(0, 101, 5):
@@ -24,9 +23,7 @@ try:
             for brightness in range(100, -1, -5):
                 pwm.ChangeDutyCycle(brightness)
                 time.sleep(0.1)
-        pwm.ChangeDutyCycle(0)  # Make sure the LED is turned off before moving to the next one
+        pwm.stop()
+
 finally:
-    pwm_red.stop()
-    pwm_green.stop()
-    pwm_blue.stop()
     GPIO.cleanup()
